8e3d22796ce6492cf5220c90c856bcd128f0094c
jck0day <jck0day@gmail.com>
2023-02-14 18:20:13 +0100

    First release

---
+++ app/src/main/java/com/chacha/igexperiments/MainActivity.java
@@ -0,0 +1,62 @@
+package com.chacha.igexperiments;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.io.DataOutputStream;
+import java.util.Objects;
+
+import eu.chainfire.libsuperuser.Shell;
+
+public class MainActivity extends AppCompatActivity {
+    String IG_PACKAGE_NAME = "com.instagram.android";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if(!isModuleActive()){
+            Toast.makeText(this, "Module DISABLED !", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "Module enabled !", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private static boolean isModuleActive(){
+        return false;
+    }
+
+    public void killAction() {
+        Context context = this;
+        try {
+            if (Shell.SU.available()) {
+                Process su = Runtime.getRuntime().exec("su");
+                DataOutputStream os = new DataOutputStream(su.getOutputStream());
+                os.writeBytes("adb shell" + "\n");
+                os.flush();
+                os.writeBytes("am force-stop " + IG_PACKAGE_NAME + "\n");
+                os.flush();
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException ignored) {
+                }
+
+                //launch instagram
+                Intent LaunchIntent = context.getPackageManager().getLaunchIntentForPackage(IG_PACKAGE_NAME);
+                context.startActivity(Objects.requireNonNull(LaunchIntent));
+            }
+        }catch (Exception exception){
+            Toast.makeText(context, "Root not granted !", Toast.LENGTH_SHORT).show();
+    }}
+
+    public void killIG(View view) {
+        killAction();
+    }
+}
---
+++ app/src/main/java/com/chacha/igexperiments/Module.java
@@ -0,0 +1,91 @@
+package com.chacha.igexperiments;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XC_MethodReplacement;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.app.AndroidAppHelper;
+import android.widget.BaseAdapter;
+import android.widget.Toast;
+
+import androidx.fragment.app.FragmentActivity;
+
+import java.lang.reflect.Method;
+
+public class Module implements IXposedHookLoadPackage {
+    String IG_PACKAGE_NAME = "com.instagram.android";
+
+    @Override
+    public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) throws ClassNotFoundException {
+        if(lpparam.packageName.equals(IG_PACKAGE_NAME)) {
+            /*Method method = XposedHelpers.findMethodBestMatch(XposedHelpers.findClass("X.0uf", lpparam.classLoader), "A05",
+                    XposedHelpers.findClass("com.instagram.service.session.UserSession", lpparam.classLoader));
+*/
+            XposedHelpers.findAndHookMethod("X.0uf", lpparam.classLoader, "A05",
+                    XposedHelpers.findClass("com.instagram.service.session.UserSession", lpparam.classLoader),
+                    XC_MethodReplacement.returnConstant(true));
+            
+            XposedBridge.log("Experiments enabled!");
+
+            /*final Object[] userSession = new Object[1];
+            final Object[] fragmentActivity = new Object[1];
+
+            XposedHelpers.findAndHookConstructor("com.instagram.service.session.UserSession", lpparam.classLoader, XposedHelpers.findClass("com.instagram.user.model.User", lpparam.classLoader), XposedHelpers.findClass("X.08u", lpparam.classLoader),
+            XposedHelpers.findClass("X.0YI", lpparam.classLoader), boolean.class, XposedHelpers.findClass("X.0Y5", lpparam.classLoader), new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                    super.beforeHookedMethod(param);
+                }
+                @Override
+                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                    super.afterHookedMethod(param);
+                    Toast.makeText(AndroidAppHelper.currentApplication(), "got userSession: ", Toast.LENGTH_SHORT).show();
+                    userSession[0] = param.thisObject;
+                }
+            });
+
+            XposedHelpers.findAndHookMethod("com.instagram.mainactivity.MainActivity", lpparam.classLoader, "onStart", new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                    super.beforeHookedMethod(param);
+                }
+                @Override
+                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                    super.afterHookedMethod(param);
+                    fragmentActivity[0] = (Activity) param.thisObject;
+                    Toast.makeText(AndroidAppHelper.currentApplication(), "got fragmentActivity: " + fragmentActivity[0], Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            XposedHelpers.findAndHookConstructor("com.instagram.actionbar.ActionBarTitleViewSwitcher", lpparam.classLoader, android.content.Context.class, android.util.AttributeSet.class, new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                    super.beforeHookedMethod(param);
+                }
+                @Override
+                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                    super.afterHookedMethod(param);
+                  
+                   // Toast.makeText(AndroidAppHelper.currentApplication(), "ActionBarTitleViewSwitcher: "+ param.thisObject.getClass(), Toast.LENGTH_LONG).show();
+                    XposedHelpers.callMethod(XposedHelpers.findClass("com.instagram.debug.devoptions.api.DeveloperOptionsLauncher", lpparam.classLoader), "launchDeveloperOptionModalActivity",
+                            AndroidAppHelper.currentApplication(),
+                            activity,
+                            userSession[0],
+                            "salope");
+                }
+            });*/
+        }
+
+        if(lpparam.packageName.equals("com.chacha.igexperiments")) {
+            findAndHookMethod("com.chacha.igexperiments" + ".MainActivity", lpparam.classLoader,
+                    "isModuleActive", XC_MethodReplacement.returnConstant(true));
+        }
+    }
+}
