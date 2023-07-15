1201df25d7040decd96bd8b38a40ed93e47764c3
ReSo7200 <47777771+ReSo7200@users.noreply.github.com>
2023-07-16 00:20:51 +0300

    Update Module.java

--- app/src/main/java/com/chacha/igexperiments/Module.java
+++ app/src/main/java/com/chacha/igexperiments/Module.java
@@ -1,5 +1,6 @@
 package com.chacha.igexperiments;
 
+// imports
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XC_MethodReplacement;
@@ -14,65 +15,12 @@ public class Module implements IXposedHookLoadPackage {
     @Override
     public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) {
         if(lpparam.packageName.equals(IG_PACKAGE_NAME)) {
+            // hooking the requited method
             XposedHelpers.findAndHookMethod("X.8W4", lpparam.classLoader, "A03",
                     XposedHelpers.findClass("com.instagram.service.session.UserSession", lpparam.classLoader),
                     XC_MethodReplacement.returnConstant(true));
-
-           /* Object obj = XposedHelpers.newInstance(XposedHelpers.findClass("com.instagram.debug.quickexperiment.storage.QuickExperimentBisectStoreModel", lpparam.classLoader));
-
-            Integer ouin = (Integer) XposedHelpers.callMethod(obj,"getUniverseIndex", "is_employee");
-            XposedBridge.log("Universe index: " + ouin);*/
-
-
-            /*final Object[] userSession = new Object[1];
-            final Object[] fragmentActivity = new Object[1];
-
-            XposedHelpers.findAndHookConstructor("com.instagram.service.session.UserSession", lpparam.classLoader, XposedHelpers.findClass("com.instagram.user.model.User", lpparam.classLoader), XposedHelpers.findClass("X.08u", lpparam.classLoader),
-            XposedHelpers.findClass("X.0YI", lpparam.classLoader), boolean.class, XposedHelpers.findClass("X.0Y5", lpparam.classLoader), new XC_MethodHook() {
-                @Override
-                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                    super.beforeHookedMethod(param);
-                }
-                @Override
-                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                    super.afterHookedMethod(param);
-                    Toast.makeText(AndroidAppHelper.currentApplication(), "got userSession: ", Toast.LENGTH_SHORT).show();
-                    userSession[0] = param.thisObject;
-                }
-            });
-
-            XposedHelpers.findAndHookMethod("com.instagram.mainactivity.MainActivity", lpparam.classLoader, "onStart", new XC_MethodHook() {
-                @Override
-                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                    super.beforeHookedMethod(param);
-                }
-                @Override
-                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                    super.afterHookedMethod(param);
-                    fragmentActivity[0] = (Activity) param.thisObject;
-                    Toast.makeText(AndroidAppHelper.currentApplication(), "got fragmentActivity: " + fragmentActivity[0], Toast.LENGTH_SHORT).show();
-                }
-            });
-
-            XposedHelpers.findAndHookConstructor("com.instagram.actionbar.ActionBarTitleViewSwitcher", lpparam.classLoader, android.content.Context.class, android.util.AttributeSet.class, new XC_MethodHook() {
-                @Override
-                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                    super.beforeHookedMethod(param);
-                }
-                @Override
-                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                    super.afterHookedMethod(param);
-                  
-                   // Toast.makeText(AndroidAppHelper.currentApplication(), "ActionBarTitleViewSwitcher: "+ param.thisObject.getClass(), Toast.LENGTH_LONG).show();
-                    XposedHelpers.callMethod(XposedHelpers.findClass("com.instagram.debug.devoptions.api.DeveloperOptionsLauncher", lpparam.classLoader), "launchDeveloperOptionModalActivity",
-                            AndroidAppHelper.currentApplication(),
-                            activity,
-                            userSession[0],
-                            "salope");
-                }
-            });*/
         }
-
+            // checking if the module is active or not
         if(lpparam.packageName.equals("com.chacha.igexperiments")) {
             findAndHookMethod("com.chacha.igexperiments" + ".MainActivity", lpparam.classLoader,
                     "isModuleActive", XC_MethodReplacement.returnConstant(true));
