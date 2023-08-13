cfbea5aed7642fd7341357c91b03f86450849b59
jck0day <jck0day@gmail.com>
2023-08-13 19:59:51 +0200

    UI improvement

--- app/src/main/java/com/chacha/igexperiments/MainActivity.java
+++ app/src/main/java/com/chacha/igexperiments/MainActivity.java
@@ -2,7 +2,6 @@ package com.chacha.igexperiments;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SwitchCompat;
-
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
