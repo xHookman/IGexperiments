96f943fdde0c7ab9d99973315b9772e729f35ddd
ReSo7200 <47777771+ReSo7200@users.noreply.github.com>
2023-09-19 13:26:49 +0300

    Update Utils.java

--- app/src/main/java/com/chacha/igexperiments/Utils.java
+++ app/src/main/java/com/chacha/igexperiments/Utils.java
@@ -4,7 +4,7 @@ public class Utils {
     public static final String IG_PACKAGE_NAME = "com.instagram.android";
     public static final String MY_PACKAGE_NAME = "com.chacha.igexperiments";
     public static final String PREFS_NAME = "IGExperiments";
-    public static final String DEFAULT_CLASS_TO_HOOK = "X.8W4";
-    public static final String DEFAULT_METHOD_TO_HOOK = "A03";
-    public static final String DEFAULT_SECOND_CLASS_TO_HOOK = "com.instagram.service.session.UserSession";
+    public static final String DEFAULT_CLASS_TO_HOOK = "X.1Ay";
+    public static final String DEFAULT_METHOD_TO_HOOK = "A00";
+    public static final String DEFAULT_SECOND_CLASS_TO_HOOK = "X.0cQ";
 }
