13608d22f3869c3351694f401797cc6e70180309
Abdul <abdulhaleemtamimi@gmail.com>
2025-01-04 19:43:54 +0300

    Version 4.0

--- app/src/main/java/com/chacha/igexperiments/Utils.java
+++ app/src/main/java/com/chacha/igexperiments/Utils.java
@@ -3,8 +3,5 @@ package com.chacha.igexperiments;
 public class Utils {
     public static final String IG_PACKAGE_NAME = "com.instagram.android";
     public static final String MY_PACKAGE_NAME = "com.chacha.igexperiments";
-    public static final String PREFS_NAME = "IGExperiments";
-    public static final String DEFAULT_CLASS_TO_HOOK = "X.1XX";
-    public static final String DEFAULT_METHOD_TO_HOOK = "A00";
-    public static final String DEFAULT_SECOND_CLASS_TO_HOOK = "com.instagram.common.session.UserSession";
+    public static final String USER_SESSION_CLASS = "com.instagram.common.session.UserSession";
 }
